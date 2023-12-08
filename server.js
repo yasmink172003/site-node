ce967ed3d7a427e5fe1d7a888e42dc21e42247f4
Yasmin <126991883+yasmink172003@users.noreply.github.com>
2023-12-08 11:21:29 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+/* Mostra o valor da variável em reais pro usuário na DOM: */
+let value = 38.0;
+let price = value.toLocaleString("pt-br", {
+  style: "currency",
+  currency: "BRL"
+});
+/* --------Muda o preço do produto a partir da quantidade
+definida pelo input number na DOM: */
+let quantity = document.getElementById("productQuantity").value;
+console.log(quantity);
+
+/* --------Muda as imagens da galeria: */
+let smallImg = document.getElementsByClassName("small-img");
+function changeMainImg(source) {
+  let mainImg = document.getElementById("mainImg");
+  mainImg.src = source.src;
+}
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
